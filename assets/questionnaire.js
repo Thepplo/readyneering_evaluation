74b4e064d7c574be1896f9d52712707466a74161
Theo <theo.salminen12@gmail.com>
2026-05-17 16:00:16 +0200

    Replacing question icon

--- assets/questionnaire.js
+++ assets/questionnaire.js
@@ -2162,7 +2162,11 @@ function renderFocusActionsSection(focusActions) {
 
       <div class="focus-actions-block questions">
         <div class="focus-actions-block-header">
-          <span class="section-icon" aria-hidden="true">?</span>
+          <span class="section-icon section-icon-question" aria-hidden="true">
+            <svg viewBox="0 0 24 24">
+              <path d="M12 19H12.01M8.21704 7.69689C8.75753 6.12753 10.2471 5 12 5C14.2091 5 16 6.79086 16 9C16 10.6565 14.9931 12.0778 13.558 12.6852C12.8172 12.9988 12.4468 13.1556 12.3172 13.2767C12.1629 13.4209 12.1336 13.4651 12.061 13.6634C12 13.8299 12 14.0866 12 14.6L12 16" />
+            </svg>
+          </span>
           <strong>Sit with these questions</strong>
         </div>
         <p class="focus-question-intro">
