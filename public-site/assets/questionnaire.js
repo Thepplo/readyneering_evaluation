50a5757a65beaafe4bddce11bd3b3a81fcade0df
Theo <theo.salminen12@gmail.com>
2026-06-16 21:03:48 +0200

    Adding escapehtml

--- public-site/assets/questionnaire.js
+++ public-site/assets/questionnaire.js
@@ -70,6 +70,15 @@ async function getTurnstileToken() {
   });
 }
 
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
 function getSubmitAttemptId() {
   let id = localStorage.getItem('submit_attempt_id');
 
