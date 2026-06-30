279f12d57526f13fd30334f6e4a888b358a28368
Theo <theo.salminen12@gmail.com>
2026-06-30 15:02:11 +0200

    Changing escapeHTML

--- public-site/assets/questionnaire.js
+++ public-site/assets/questionnaire.js
@@ -132,11 +132,11 @@ function renderTurnstileWidgetOnce() {
 
 function escapeHtml(unsafe) {
   return String(unsafe ?? '')
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#039;');
 }
 
 function getSubmitAttemptId() {
