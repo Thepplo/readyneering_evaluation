a7d4231fe564db40043fffe552e1440926796af4
Theo <theo.salminen12@gmail.com>
2026-07-09 12:57:21 +0200

    Fixing anon key

--- internal-overview/assets/auth.js
+++ internal-overview/assets/auth.js
@@ -3,7 +3,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
    export const SUPABASE_URL = 'https://supabase-andqfive-u72683.vm.elestio.app';
-   export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzc4MjQwOTI0LCJleHAiOleHAiOjIwOTM2MDA5MjR9.RY78HRRVwTCZFZIlok07BQm0hMM-t9J9B8ZD_w_TW8M';
+   export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzc4MjQwOTI0LCJleHAiOjIwOTM2MDA5MjR9.RY78HRRVwTCZFZIlok07BQm0hMM-t9J9B8ZD_w_TW8M';
 
    export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
